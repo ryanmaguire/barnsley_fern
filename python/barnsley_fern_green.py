2e48649169f31c109a460ce0a4389d2abb725ab7
ryanmaguire <rmaguire@protonmail.com>
2023-10-11 16:45:37 -0400

    More code.

---
+++ python/barnsley_fern_green.py
@@ -0,0 +1,28 @@
+"""
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is part of barnsley_fern.                                        #
+#                                                                              #
+#   barnsley_fern is free software: you can redistribute it and/or modify it   #
+#   under the terms of the GNU General Public License as published by          #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   barnsley_fern is distributed in the hope that it will be useful,           #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with barnsley_fern.  If not, see <https://www.gnu.org/licenses/>.    #
+################################################################################
+#   Purpose:                                                                   #
+#       Creates a Barnsley fern.                                               #
+################################################################################
+#   Author:     Ryan Maguire                                                   #
+#   Date:       October 11, 2023.                                              #
+################################################################################
+"""
+import barnsley
+barnsley.run(barnsley.color.greenscale, "barnsley_fern_green.ppm")
